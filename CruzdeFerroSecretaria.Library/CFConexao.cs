2c3a0950c32fed5426dedf50cb5cd2698aa409d9
Eduardo Maia <eduvimaia@gmail.com>
2012-01-10 00:35:59 -0200

    refactoring nas conexoes com db

---
+++ CruzdeFerroSecretaria.Library/CFConexao.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Data.SqlClient;
+
+namespace CruzdeFerroSecretaria.Library
+{
+    class CFConexao
+    {
+        private const String UserId = "";
+        private const String Password = "";
+        private const String Database = "36Manager_testes";
+        private const String DataSource = "(local)";
+        
+        // Para Windows Authentication Mode, user "Trusted_Connection=Yes";
+        private const String ConnectionString = "Trusted_Connection=Yes;packet size=4096;persist security info=TRUE;initial catalog=" + Database + ";user id=" + UserId + ";password=" + Password + ";data source=" + DataSource;
+
+        private static SqlConnection oSqlConnection = null;
+
+        public static SqlConnection Conexao { get { return getConexao(); } }
+
+        private static SqlConnection getConexao()
+        {
+            if (oSqlConnection == null)
+            {
+                oSqlConnection = new SqlConnection(ConnectionString);
+            }
+
+            oSqlConnection.Open();
+            return oSqlConnection;
+        }
+
+        public static SqlDataReader ExecuteSelect(string SQL)
+        {
+            SqlCommand oSqlCommand = new SqlCommand(SQL, Conexao);
+            SqlDataReader oReader = oSqlCommand.ExecuteReader();
+
+            return oReader;
+        }
+
+        public static void ExecSQL(string SQL)
+        {          
+            SqlCommand oSqlCommand = new SqlCommand();
+
+            oSqlCommand = new SqlCommand(SQL, Conexao);
+            oSqlCommand.ExecuteNonQuery();
+
+            Conexao.Close();
+        }
+
+    }
+}
--- CruzdeFerroSecretaria.Library/FaccaoDAO.cs
+++ CruzdeFerroSecretaria.Library/FaccaoDAO.cs
@@ -16,12 +16,6 @@ namespace CruzdeFerroSecretaria.Library
         /// <param name="oFaccao">Objeto de Faccao</param>
         public void SalvarFaccao(Faccao oFaccao)
         {
-            //Conexao
-            SqlConnection oSqlConnection = new SqlConnection("packet size=4096;persist security info=TRUE;initial catalog=CF_Secretaria;user id=sa;password=123456;data source=LOVIEIRA-PC\\SQL_SOLUTIO42");
-            oSqlConnection.Open();
-
-            SqlCommand oSqlCommand = new SqlCommand();
-
             short PossuiSede = 0;
             if (oFaccao.PossuiSede)
                 PossuiSede = 1;
@@ -30,15 +24,11 @@ namespace CruzdeFerroSecretaria.Library
 
             //Sql de update dos Eventos
             if (oFaccao.FaccaoID != 0)
-                oSqlCommand = new SqlCommand(String.Format("UPDATE cfFaccao SET cfFaccaoCidade = '{0}', cfFaccaoEmail = '{1}', cfFaccaoPossuiSede = {2},cfFaccaoLogradouro = '{3}',cfFaccaoCep = '{4}',cfFaccaoEstado = '{5}' WHERE cfFaccaoID = {6}", oFaccao.EnderecoSede.Cidade, oFaccao.Email, PossuiSede, oFaccao.EnderecoSede.Logradouro, oFaccao.EnderecoSede.CEP, oFaccao.EnderecoSede.Estado, oFaccao.FaccaoID), oSqlConnection);
+                CFConexao.ExecSQL(String.Format("UPDATE cfFaccao SET cfFaccaoCidade = '{0}', cfFaccaoEmail = '{1}', cfFaccaoPossuiSede = {2},cfFaccaoLogradouro = '{3}',cfFaccaoCep = '{4}',cfFaccaoEstado = '{5}' WHERE cfFaccaoID = {6}", oFaccao.EnderecoSede.Cidade, oFaccao.Email, PossuiSede, oFaccao.EnderecoSede.Logradouro, oFaccao.EnderecoSede.CEP, oFaccao.EnderecoSede.Estado, oFaccao.FaccaoID));
 
             //Sql de Inserção dos Eventos
             else
-                oSqlCommand = new SqlCommand(String.Format("INSERT INTO cfFaccao VALUES('{0}','{1}',{2},'{3}','{4}','{5}')", oFaccao.EnderecoSede.Cidade, oFaccao.Email, PossuiSede, oFaccao.EnderecoSede.Logradouro, oFaccao.EnderecoSede.CEP, oFaccao.EnderecoSede.Estado), oSqlConnection);
-            
-            oSqlCommand.ExecuteNonQuery();
-
-            oSqlConnection.Close();
+                CFConexao.ExecSQL(String.Format("INSERT INTO cfFaccao VALUES('{0}','{1}',{2},'{3}','{4}','{5}')", oFaccao.EnderecoSede.Cidade, oFaccao.Email, PossuiSede, oFaccao.EnderecoSede.Logradouro, oFaccao.EnderecoSede.CEP, oFaccao.EnderecoSede.Estado));
         }
 
         /// <summary>
@@ -48,13 +38,8 @@ namespace CruzdeFerroSecretaria.Library
         /// <returns>Lista de Faccaos Retornados</returns>
         public List<Faccao> GetAllFaccao()
         {
-            //Conexao
-            SqlConnection oSqlConnection = new SqlConnection("packet size=4096;persist security info=TRUE;initial catalog=CF_Secretaria;user id=sa;password=123456;data source=LOVIEIRA-PC\\SQL_SOLUTIO42");
-            oSqlConnection.Open();
-
             //Sql de busca dos Faccaos
-            SqlCommand oSqlCommand = new SqlCommand("SELECT * FROM cfFaccao", oSqlConnection);
-            SqlDataReader oReader = oSqlCommand.ExecuteReader();
+            SqlDataReader oReader = CFConexao.ExecuteSelect("SELECT * FROM cfFaccao");
 
             //Retornando os Eventos
             List<Faccao> oListFaccaos = new List<Faccao>();
@@ -75,7 +60,7 @@ namespace CruzdeFerroSecretaria.Library
             }
 
             oReader.Close();
-            oSqlConnection.Close();
+            CFConexao.Conexao.Close();
 
             return oListFaccaos;
         }
@@ -87,16 +72,8 @@ namespace CruzdeFerroSecretaria.Library
         /// <param name="oEvento">Objeto de Faccao</param>
         public void ExcluirFaccao(Faccao oFaccao)
         {
-            //Conexao
-            SqlConnection oSqlConnection = new SqlConnection("packet size=4096;persist security info=TRUE;initial catalog=CF_Secretaria;user id=sa;password=123456;data source=LOVIEIRA-PC\\SQL_SOLUTIO42");
-            oSqlConnection.Open();
-
             //Sql de excluir das Faccoes
-            SqlCommand oSqlCommand = new SqlCommand(String.Format("DELETE FROM cfFaccao WHERE cfFaccaoID = {0}", oFaccao.FaccaoID), oSqlConnection);
-
-            oSqlCommand.ExecuteNonQuery();
-
-            oSqlConnection.Close();
+            CFConexao.ExecSQL(String.Format("DELETE FROM cfFaccao WHERE cfFaccaoID = {0}", oFaccao.FaccaoID));
         }
         #endregion
     }
